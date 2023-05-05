6ab896f5c99dc92a0679048c8878a6392c000f54
Changmin Choi <cmchoi9901@gmail.com>
2023-05-06 02:59:39 +0900

    remove validation for node 4

--- hw3/matmul/run_performance.sh
+++ hw3/matmul/run_performance.sh
@@ -2,4 +2,4 @@
 
 NODES=1 ./run.sh -n 10 -v -t 32 4096 4096 4096
 NODES=2 ./run.sh -n 10 -v -t 32 8192 4096 4096
-NODES=4 ./run.sh -n 10 -v -t 32 16384 4096 4096
+NODES=4 ./run.sh -n 10 -t 32 16384 4096 4096
