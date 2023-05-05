9894b741626a85a6d87ec0d14b45acda3a1ec977
Changmin Choi <cmchoi9901@gmail.com>
2023-05-06 02:53:10 +0900

    WIP: make matmul working

--- hw3/matmul/matmul.c
+++ hw3/matmul/matmul.c
@@ -12,22 +12,6 @@ __m512 __fma(__m512 a, __m512 b, __m512 c, __m512 curr) {
   return _mm512_add_ps(curr, _mm512_fmadd_ps(a, b, c));
 }
 
-float hadd(__m512 v) {
-  //    1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
-  // -> 4 3 2 1 8 7 6 5
-  __m512 curr = _mm512_shuffle_ps(v, v, 0b00011011);
-  // 5 5 5 5 13 13 13 13
-  curr = _mm512_add_ps(v, curr);
-  // 10 26 10 26 10 26 10 26
-  curr = _mm512_hadd_ps(curr, curr);
-  // 36 36 36 36 36 36 36 36 
-  curr = _mm512_hadd_ps(curr, curr);
-
-  float ret;
-  _mm512_mask_store_ps(&ret, 1, curr);
-  return ret;
-}
-
 void matmul(float *A, float *B, float *C, int M, int N, int K,
     int threads_per_process, int mpi_rank, int mpi_world_size) {
   // TODO: FILL_IN_HERE
@@ -47,11 +31,11 @@ void matmul(float *A, float *B, float *C, int M, int N, int K,
   }
 
   // Data Transfer: A
-  MPI_Bcast(&Bp, N * K, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(Bp, N * K, MPI_FLOAT, 0, MPI_COMM_WORLD);
   // Data Transfer: B
   int chunksize = M / mpi_world_size;
   float *Ac, *Cc;
-  __m512 Ccc[N * chunksize];
+  __m512 *Ccc = aligned_alloc(512, N * chunksize * sizeof(__m512));
   alloc_mat(&Ac, K, chunksize);
   alloc_mat(&Cc, N, chunksize);
   MPI_Scatter(A, K * chunksize, MPI_FLOAT, Ac, K * chunksize, MPI_FLOAT, 0, MPI_COMM_WORLD);
@@ -59,10 +43,9 @@ void matmul(float *A, float *B, float *C, int M, int N, int K,
   __m512 a;
   __m512 b;
   __m512 c;
-  __m512 curr;
   
   omp_set_num_threads(threads_per_process);
-  #pragma omp parallel private(a, b, c, curr)
+  #pragma omp parallel private(a, b, c)
   {
     #pragma omp for nowait
     for (int m = 0; m < chunksize; ++m) {
@@ -75,9 +58,12 @@ void matmul(float *A, float *B, float *C, int M, int N, int K,
         }
       }
     }
+
+    #pragma omp for nowait
+    for (int i = 0; i < chunksize * N; ++i) {
+      Cc[i] = _mm512_reduce_add_ps(Ccc[i]);
+    }
   }
-
-  // TODO Convert Ccc to Cc
-
+  free(Ccc);
   MPI_Gather(Cc, N * chunksize, MPI_FLOAT, C, N * chunksize, MPI_FLOAT, 0, MPI_COMM_WORLD);
 }
