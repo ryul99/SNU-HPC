863be4d47b232e02b6fcf996ea5af237f0c949cb
Changmin Choi <cmchoi9901@gmail.com>
2023-05-06 03:49:47 +0900

    WIP: hw3 matmul.c

--- hw3/matmul/matmul.c
+++ hw3/matmul/matmul.c
@@ -15,14 +15,26 @@ __m512 __fma(__m512 a, __m512 b, __m512 c, __m512 curr) {
 void matmul(float *A, float *B, float *C, int M, int N, int K,
     int threads_per_process, int mpi_rank, int mpi_world_size) {
   // TODO: FILL_IN_HERE
+  omp_set_num_threads(threads_per_process);
+  __m512 a;
+  __m512 b;
+  __m512 c;
+  __m512 curr;
+  int chunksize = M / mpi_world_size;
+  MPI_Request r[2];
+  float *Ac, *Bp, *Cc;
+  alloc_mat(&Ac, K, chunksize);
+  alloc_mat(&Bp, N, K);
+  alloc_mat(&Cc, N, chunksize);
+  MPI_Iscatter(A, K * chunksize, MPI_FLOAT, Ac, K * chunksize, MPI_FLOAT, 0, MPI_COMM_WORLD, &r[1]);
+
   // A: M x K
   // B: K x N
   // C: M x N
 
   // Bp: N x K
-  float *Bp;
-  alloc_mat(&Bp, N, K);
   if (mpi_rank == 0) {
+    #pragma omp parallel for
     for (int k = 0; k < K; ++k) {
       for (int n = 0; n < N; ++n) {
         Bp[k + K * n] = B[n + N * k];
@@ -30,40 +42,29 @@ void matmul(float *A, float *B, float *C, int M, int N, int K,
     }
   }
 
-  // Data Transfer: A
-  MPI_Bcast(Bp, N * K, MPI_FLOAT, 0, MPI_COMM_WORLD);
-  // Data Transfer: B
-  int chunksize = M / mpi_world_size;
-  float *Ac, *Cc;
-  __m512 *Ccc = aligned_alloc(512, N * chunksize * sizeof(__m512));
-  alloc_mat(&Ac, K, chunksize);
-  alloc_mat(&Cc, N, chunksize);
-  MPI_Scatter(A, K * chunksize, MPI_FLOAT, Ac, K * chunksize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  // Data Transfer
+  MPI_Ibcast(Bp, N * K, MPI_FLOAT, 0, MPI_COMM_WORLD, &r[0]);
+  MPI_Waitall(2, r, MPI_STATUS_IGNORE);
 
-  __m512 a;
-  __m512 b;
-  __m512 c;
   
-  omp_set_num_threads(threads_per_process);
-  #pragma omp parallel private(a, b, c)
+  #pragma omp parallel private(a, b, c, curr)
   {
     #pragma omp for nowait
     for (int m = 0; m < chunksize; ++m) {
       for (int n = 0; n < N; ++n) {
         c = _mm512_set1_ps(Cc[n + N * m]);
+        curr = _mm512_set1_ps(0);
         for (int k = 0; k < K; k += 16) {
           a = _mm512_load_ps(&Ac[k + K * m]);
           b = _mm512_load_ps(&Bp[k + K * n]);
-          Ccc[n + N * m] = __fma(a, b, c, Ccc[n + N * m]);;
+          curr = __fma(a, b, c, curr);;
         }
+        Cc[n + N * m] = _mm512_reduce_add_ps(curr);
       }
     }
-
-    #pragma omp for nowait
-    for (int i = 0; i < chunksize * N; ++i) {
-      Cc[i] = _mm512_reduce_add_ps(Ccc[i]);
-    }
   }
-  free(Ccc);
   MPI_Gather(Cc, N * chunksize, MPI_FLOAT, C, N * chunksize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  free(Ac);
+  free(Bp);
+  free(Cc);
 }
