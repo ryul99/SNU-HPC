c9bb50ebbceb4e60023fd8f8053042f8624ba7bf
Changmin Choi <cmchoi9901@gmail.com>
2023-05-06 18:31:30 +0900

    remove nowait

--- hw3/matmul/matmul.c
+++ hw3/matmul/matmul.c
@@ -50,13 +50,13 @@ void matmul(float *A, float *B, float *C, int M, int N, int K,
   MPI_Scatter(B, chunkK * N, MPI_FLOAT, Bc, chunkK * N, MPI_FLOAT, 0, MPI_COMM_WORLD);
   #pragma omp parallel private(curr)
   {
-    #pragma omp for nowait
+    #pragma omp for
     for (int k = 0; k < chunkK; ++k) {
       for (int n = 0; n < N; ++n) {
         Bp[k + chunkK * n] = Bc[n + N * k];
       }
     }
-    #pragma omp for nowait
+    #pragma omp for
     for (int mm = 0; mm < M; mm += sz) {
       for (int nn = 0; nn < N; nn += sz) {
         for (int m = mm; m < MIN(mm + sz, M); ++m) {
