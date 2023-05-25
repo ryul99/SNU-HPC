9db3d8722c92877ac603d313627fb64ba060a26c
Changmin Choi <cmchoi9901@gmail.com>
2023-05-25 13:56:36 +0900

    init hw5

---
+++ hw5/matmul/matmul.cu
@@ -0,0 +1,17 @@
+#include "matmul.h"
+#include "util.h"
+
+#include <cuda_runtime.h>
+#include <mpi.h>
+
+void matmul(const float *A, const float *B, float *C, int M, int N, int K) {
+  // TODO: FILL_IN_HERE
+}
+
+void matmul_initialize(int M, int N, int K) {
+  // TODO: FILL_IN_HERE
+}
+
+void matmul_finalize() {
+  // TODO: FILL_IN_HERE
+}
