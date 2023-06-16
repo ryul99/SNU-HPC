86ca58614f8b9e73dda1d4adf0b9dbf9488cfff1
Changmin Choi <cmchoi9901@gmail.com>
2023-06-16 13:43:39 +0900

    add run.sh

--- final-project/run.sh
+++ final-project/run.sh
@@ -2,7 +2,12 @@
 
 : ${NODES:=1}
 
-srun -N $NODES --exclusive --partition=shpc          \
-  mpirun --bind-to none -mca btl ^openib -npernode 1 \
-  numactl --physcpubind 0-63                         \
-  /usr/local/cuda/bin/ncu --set full ./translator $@
+salloc -N $NODES --exclusive --partition=shpc --gres=gpu:4                    \
+  mpirun --bind-to none -mca btl ^openib -npernode 1                          \
+  numactl --physcpubind 0-63                                                  \
+  ./translator -n 1024 -v $@
+
+  # /usr/local/cuda/bin/ncu --set full 
+  # /usr/local/cuda/bin/nsys profile --gpu-metrics-device 0    \
+  # --cudabacktrace=all --force-overwrite true -o profile.qdrep \
+  # --trace=mpi,cuda            \
